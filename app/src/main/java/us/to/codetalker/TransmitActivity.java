e79c333b00115522a3ba9ebcc6f040305acb6e94
Alexander Desmond <alexdesmond@Knights.ucf.edu>
2019-03-24 00:59:29 -0400

    Determined which cameraId has flash, output result to Logcat

--- app/src/main/java/us/to/codetalker/TransmitActivity.java
+++ app/src/main/java/us/to/codetalker/TransmitActivity.java
@@ -5,7 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.*;
+import android.util.Log;
 
 public class TransmitActivity extends AppCompatActivity {
 
@@ -20,17 +21,27 @@ public class TransmitActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String flashCameraId;
 
                 CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
                 try {
                     for (String cameraId : cameraManager.getCameraIdList()) {
-                        System.out.print(cameraId);
+                        CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                        Boolean hasFlash = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                        if (hasFlash != null && hasFlash) { // If this camera ID has flash
+                            flashCameraId = cameraId;
+                            Log.i("HARDCODE",flashCameraId + " Has Flash");
+                        }
+
 
                     }
                 }
-                catch (android.hardware.camera2.CameraAccessException e){
+                catch (CameraAccessException e){
                     e.printStackTrace();
                 }
+
+
+
           //      Camera cam = Camera.open();
           //      Parameters p = cam.getParameters();
           //      p.setFlashMode(Parameters.FLASH_MODE_TORCH);
