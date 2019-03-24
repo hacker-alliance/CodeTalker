9b98386d1612864406044999d9bfd4f098a278a6
Alexander Desmond <alexdesmond@Knights.ucf.edu>
2019-03-24 02:14:57 -0400

    Created Transmit function to which one can output a boolean ArrayList

--- app/src/main/java/us/to/codetalker/TransmitActivity.java
+++ app/src/main/java/us/to/codetalker/TransmitActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.hardware.camera2.*;
 import android.util.Log;
+import android.os.Handler;
+import java.util.ArrayList;
 
 public class TransmitActivity extends AppCompatActivity {
 
@@ -17,13 +19,16 @@ public class TransmitActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String flashCameraId;
 
-                CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+             final  CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+             final  String flashCameraId;
+
                 try {
                     for (String cameraId : cameraManager.getCameraIdList()) {
                         CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
@@ -31,8 +36,8 @@ public class TransmitActivity extends AppCompatActivity {
                         if (hasFlash != null && hasFlash) { // If this camera ID has flash
                             flashCameraId = cameraId;
                             Log.i("HARDCODE",flashCameraId + " Has Flash");
+                            break;
                         }
-
 
                     }
                 }
@@ -40,15 +45,55 @@ public class TransmitActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-
-
-          //      Camera cam = Camera.open();
-          //      Parameters p = cam.getParameters();
-          //      p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-          //      cam.setParameters(p);
-          //      cam.startPreview();
             }
+
+
         });
     }
 
+    public void toggleFlash(CameraManager cameraManager, String flashCameraId, boolean status){
+        try {
+            cameraManager.setTorchMode(flashCameraId, status);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Transmit(final ArrayList<Boolean> message, final CameraManager cameraManager, final String displayCameraId ){
+
+        for(int i = 0; i < message.size() ; i++) {
+            if (message.get(i)) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        try {
+                            cameraManager.setTorchMode(displayCameraId, true);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, 100); // Millisecond 100
+            }
+            else{
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        try {
+                            cameraManager.setTorchMode(displayCameraId, false);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, 100); // Millisecond 100
+            }
+        }
+    }
+
 }
+
